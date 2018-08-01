255e5b8d0e80a05492e9edacea7984867ff6808e
Francisca van maurik <franciscavanmaurik@gmail.com>
2018-08-01 19:48:58 +0200

    responsive3

--- sw.js
+++ sw.js
@@ -37,7 +37,8 @@ self.addEventListener('install', function(event) {
 
  
 
-
+/* Mozilla https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+was a usefull source in understanding this*/
 
   
   
